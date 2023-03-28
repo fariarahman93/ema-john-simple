c7d551978b2add4e2c60fa825af40c65c5f693f3
faria-rahman <fariarahman.tora@gmail.com>
2023-03-29 00:13:34 +0600

    emaJhon updated

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -8,17 +8,24 @@ const Shop = () => {
         .then(res=>res.json())
         .then(data=>setProducts(data));
     },[])
+    const [cart,setCart]=useState([]);
+    const handleAddToCart=(product)=>{
+        const newCart= [...cart,product];
+        setCart(newCart);
+        console.log('added to cart:',cart);
+    }
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
                     products.map(product=>{
-                        return <Product product={product} key={product.id}></Product>
+                        return <Product product={product} key={product.id} handleAddToCart={handleAddToCart}> </Product>
                     })
                 }
             </div>
             <div className="cart-container">
                 <h4>Order Summary</h4>
+                <p>Selected Item: {cart.length}</p>
             </div>
         </div>
     );
